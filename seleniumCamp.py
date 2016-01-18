605693913dd4c47e186c14ca93cf4f2fb2439294
neavemj <matthewjneave1@gmail.com>
2016-01-19 10:11:09 +1100

    change_date and helper functions implemented

--- seleniumCamp.py
+++ seleniumCamp.py
@@ -7,16 +7,27 @@ from selenium import webdriver
 driver = webdriver.Firefox()
 driver.get("http://www.parkstay.vic.gov.au/great-otway-national-park-west-kennett-river-to-princetown")
 
+# helper functions
+
+def select_in_dropdown(menu, selection):
+    for option in menu.find_elements_by_tag_name("option"):
+        if option.text == selection:
+            option.click()
+            return
+
 # change date
 
-def change_date(new_date):
+def change_date(day, month_year):
     driver.find_element_by_class_name("pseudo").click()
     date_picker = driver.find_element_by_class_name("wdDatePicker_calendar")
-    rows = date_picker.find_element_by_tag_name("tr")
-    columns = date_picker.find_element_by_tag_name("td")
-    for cell in columns:
-        if cell.text == "20":
-            cell.click()
+
+    dropdown = date_picker.find_element_by_tag_name("select")
+    select_in_dropdown(dropdown, month_year)
+
+    # need re-find the calendar element to avoid "element no longer attached to DOM" error
+    date_picker = driver.find_element_by_class_name("wdDatePicker_calendar")
+    day_elem = date_picker.find_elements_by_xpath("//b[text()='%s']" % day)
+    day_elem[0].click()
 
 # check if site booked
 
@@ -31,4 +42,4 @@ def check_if_booked(site):
             return False
 
 
-change_date("none")
+change_date("28", "July 2016")
